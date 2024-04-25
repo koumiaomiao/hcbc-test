08fbf98c25ba8b593b081435158937e23ff71974
mjkou <mjkou@thoughtworks.com>
2024-04-25 13:48:16 +0800

    init

---
+++ src/main/java/com/kmj/hcbc/HcbcApplication.java
@@ -0,0 +1,13 @@
+package com.kmj.hcbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kmj/hcbc/HcbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmj.hcbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
